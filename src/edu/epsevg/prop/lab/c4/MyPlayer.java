7e7ca408bdbf095cbebbe15d7b34651a9ab6abf9
maryupc <mary.ojuyenum@estudiantat.upc.edu>
2024-11-15 09:18:21 +0100

    prueba

---
+++ src/edu/epsevg/prop/lab/c4/MyPlayer.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.epsevg.prop.lab.c4;
+
+/**
+ *
+ * @author maryx
+ */
+public class MyPlayer
+implements Jugador, IAuto{
+
+    @Override
+    public int moviment(Tauler t, int color) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String nom() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
